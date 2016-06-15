b645f53784d1039d9a9210eea2b835498cd334b8
Sean Whalen <whalenster@gmail.com>
2016-06-15 18:20:37 -0400

    Fix sample submission

--- cuckoo.py
+++ cuckoo.py
@@ -22,7 +22,7 @@ from re import match
 from requests import session
 from io import BytesIO
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 hash_types = {"md5": md5, "sha1": sha1, "sha256": sha256}
 hash_regex = {r'[a-fA-F\d]{32}': "md5", r"[a-fA-F\d]{40}": "sha1", r"[a-fA-F\d]{64}": "sha256"}
@@ -58,7 +58,7 @@ class Cuckoo(object):
             if results["error"]:
                 raise RuntimeError(results["error_value"])
 
-    def __init__(self, cuckoo_root, username=None, password=None, verify=False, proxies=None):
+    def __init__(self, cuckoo_root, username=None, password=None, verify=True, proxies=None):
         self.root = cuckoo_root
         self.api_root = "{0}/api".format(self.root)
         self.username = username
@@ -81,7 +81,7 @@ class Cuckoo(object):
         files = dict(file=(file_name, file_to_upload))
         response = self.session.post(url, files=files,
                                      data=data)
-        return response.json()["task_ids"]
+        return response.json()["data"]["task_ids"]
 
     def submit_url(self, url, tags=None, options=None):
         if tags is None:
@@ -94,7 +94,7 @@ class Cuckoo(object):
         api_url = "{0}{1}".format(self.api_root, "/tasks/create/url/")
         data = dict(url=url, tags=tags, options=options)
         response = self.session.post(api_url, data=data)
-        return response.json()["task_ids"]
+        return response.json()["data"]["task_ids"]
 
     def submit_vtdl(self, file_hash, tags=None, options=None):
         if tags is None:
@@ -107,7 +107,7 @@ class Cuckoo(object):
         api_url = "{0}{1}".format(self.api_root, "/tasks/create/vtdl/")
         data = dict(vtdl=file_hash, tags=tags, options=options)
         response = self.session.post(api_url, data=data)
-        return response.json()['task_ids']
+        return response.json()["data"]['task_ids']
 
     def find_tasks(self, sample_hash):
         hash_type = get_hash_type(sample_hash)
